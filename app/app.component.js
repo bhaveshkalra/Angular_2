38b40eb70cfa2195a7fc1d576e7e03b57bb27fdd
bhavesh kalra <bhavesh.kalra92@gmail.com>
2017-08-14 23:49:31 +0530

    linked the first component to the root app

--- app/app.component.js
+++ app/app.component.js
@@ -9,16 +9,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var name_component_1 = require('./name.component');
+var practice_component_1 = require('./practice.component');
+var in_out_component_1 = require('./in-out.component');
 var AppComponent = (function () {
     function AppComponent() {
+        this.apply = true;
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello World </h1>\n             <h2>About me</h2>\n              <my-name></my-name>,\n                ",
-            styles: ["\nh2{\n  color:green;\n  font-size:40px;\n  font-weight:bold;\n}"],
-            directives: [name_component_1.NameComponent]
+            template: "<h1>Hello World </h1>\n             <h2>About me</h2>\n              <my-name></my-name>\n              <h1>Parent component</h1>\n              <label>enter parent app component value</label>\n              <input type=\"text\" #parenttxt (keyup)=\"0\">\n              <p>The child component value is:</p>\n              <p [class.myclass]=\"apply\">{{childData}}</p>\n              <my-in-out (childevent)=\"childData=$event\" [parentData]=\"parenttxt.value\"></my-in-out>",
+            styles: ["\nh2{\n  color:green;\n  font-size:40px;\n  font-weight:bold;\n}\n.myclass{\n    color:red;\n    font:bold 30px tahoma;\n}"],
+            directives: [[practice_component_1.PracticeComponent], [in_out_component_1.InOutComponent]]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
-import { NameComponent } from './name.component';
+import { PracticeComponent } from './practice.component';
+import { InOutComponent } from './in-out.component';
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello World </h1>
              <h2>About me</h2>
-              <my-name></my-name>,`
+              <my-name></my-name>
+              <h1>Parent component</h1>
+              <label>enter parent app component value</label>
+              <input type="text" #parenttxt (keyup)="0">
+              <p>The child component value is:</p>
+              <p [class.myclass]="apply">{{childData}}</p>
+              <my-in-out (childevent)="childData=$event" [parentData]="parenttxt.value"></my-in-out>`,
 styles: [`
 h2{
   color:green;
   font-size:40px;
   font-weight:bold;
-}`]
-  directives: [NameComponent] 
+}
+.myclass{
+    color:red;
+    font:bold 30px tahoma;
+}`],
+  directives: [ [PracticeComponent] , [InOutComponent] ]
 })
-export class AppComponent { }
+export class AppComponent { 
+  public childData:string;
+  public apply=true;
+}
